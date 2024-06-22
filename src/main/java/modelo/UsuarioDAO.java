10c7c6cb3423884b58218216f84a834c0fae6549
anabanana <ana.vallejops18@gmail.com>
2024-06-21 22:01:38 -0400

    actualizado

--- src/main/java/modelo/UsuarioDAO.java
+++ src/main/java/modelo/UsuarioDAO.java
@@ -13,5 +13,27 @@ public class UsuarioDAO {
 	PreparedStatement ps;
 	ResultSet rs;
 	
+	public Usuario validar(String nombre, String contraseña) {
+		Usuario em=new Usuario();
+		String sql="select * from Usuario where nombre=? , contraseña=?";
+		try {
+			con=cn.establecerConexion();
+			ps=con.prepareStatement(sql);
+			ps.setString(1, nombre);
+			ps.setString(2, contraseña);
+			rs=ps.executeQuery();
+			while(rs.next()) {
+				em.setNombre(rs.getString("nombre"));
+				em.setContraseña(rs.getString("contraseña"));
+				
+			}
+			
+		} catch (Exception e) {
+			
+		}
+		return em;
+		
+		
+	}
 	
 }
